c3bd9a5cf400dd4bcc0636d37cb14ab8dbea23ad
Bjorn Harvold <bjorn@harvold.com>
2022-09-07 10:05:46 +0700

    :sparkles: feat(WSJ-16): Created automated deployment script for staging and production

--- downloadLatestOpenApiSpecs.bash
+++ downloadLatestOpenApiSpecs.bash
@@ -6,32 +6,41 @@
 # Downloads all Open API spec files from
 #
 
-retrieveOpenApiSpecFunction () {
+retrieveOpenApiSpecFunction() {
   url=$1
   location=$2
 
   echo "Health check for: $url..."
 
-  contentType=`curl -s -I ${url} | grep -i "^Content-Type:"`
+  contentType=$(curl -s -I ${url} | grep -i "^Content-Type:")
 
   if [[ $contentType == *"application/json"* ]]; then
     echo "It's OK!"
-    else
-    	echo "Invalid url. Returned: $contentType"
-    	exit 1
+  else
+    echo "Invalid url. Returned: $contentType"
+    exit 1
   fi
 
   echo "Copying the contents of the URL to $location"
 
-  curl ${url} > $location
+  curl ${url} >$location
 
   echo "Download of $url complete"
 }
+
+# whether to grab the files from staging or production servers
+env=$1
 
 echo "Download latest deployed Open API specs..."
 winkUrl="https://api.wink.travel"
 integrationsUrl="https://integrations.wink.travel"
 tripPayUrl="https://api.trippay.io"
+
+if [[ $env == *"staging"* ]]; then
+  winkUrl="https://staging-api.wink.travel"
+  integrationsUrl="https://staging-integrations.wink.travel"
+  tripPayUrl="https://staging-api.trippay.io"
+fi
 
 echo "Grabbing Affiliate spec..."
 affiliateUrl="$winkUrl/v3/api-docs/affiliate"
--- publishLatestSnapshot.bash
+++ publishLatestSnapshot.bash
@@ -4,9 +4,13 @@
 # Copyright (c) 2021. wink.travel. All rights Reserved.
 #
 
-echo "Releasing wink-sdk-java..."
+echo "Publishing wink-sdk-java SNAPSHOT..."
 
-echo "Cleaning artifacts..."
+./downloadLatestOpenApiSpecs.bash staging
+
+git commit -a -m ":bookmark: build: Updated Open API files"
+
+echo "Building artifacts..."
 mvn clean compile
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
@@ -14,54 +18,10 @@ if [ $STATUS -ne 0 ]; then
   exit 1
 fi
 
-echo "Sync-ing remote master with local"
-git checkout master
-git pull
-
-git checkout develop
-newVersion=`npx git-changelog-command-line --print-next-version --major-version-pattern BREAKING --minor-version-pattern feat`
-echo "New semantic version using Conventional Commits: $newVersion"
-
-echo "Setting the next snapshot version"
-mvn versions:set -DnewVersion="$newVersion-SNAPSHOT" -DgenerateBackupPoms=false
-
-git commit -a -m ":bookmark: build: Committing updated pom.xml files"
-
-echo "Starting release process..."
-
-mvn -B gitflow:release-start gitflow:release-finish -DskipTestProject=true
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-  echo "Something went wrong on line: ${BASH_LINENO[*]}"
-  exit 1
-fi
-
-echo "Release complete. Finishing up..."
-
-echo "Pushing master to origin"
-git checkout master
-
-echo "Updating CHANGELOG.md..."
-mvn git-changelog-maven-plugin:git-changelog
-git commit -a -m ":memo: doc: Updated CHANGELOG.md..."
-
-git push origin master:refs/heads/master
-
-echo "Creating GitHub release..."
-gh release create v$newVersion --notes "See CHANGELOG.md for release notes" --target master
-
-echo "Pushing release artifacts to Sonatype..."
-mvn deploy -Psonatype-oss-release
-
-git checkout develop
-
-echo "Merging CHANGELOG.md from master..."
-git merge master --no-edit -m ":twisted_rightwards_arrows: doc: merged CHANGELOG.md from master into develop branch" --strategy-option theirs
-
 echo "Pushing develop to origin"
 git push origin develop:refs/heads/develop
 
 echo "Pushing snapshot artifacts to Sonatype..."
 mvn deploy -Psonatype-oss-release
 
-echo "Release SUCCESS"
+echo "Publishing wink-sdk-java SNAPSHOT SUCCESS"
