dc6c147a928e94437d15c714b43bf117a8ebd8d4
Bjorn Harvold <bjorn@harvold.com>
2022-09-15 14:57:16 +0700

    :construction: build: updated download script

--- releaseToMaster.bash
+++ releaseToMaster.bash
@@ -16,7 +16,7 @@ git checkout develop
 ./downloadLatestOpenApiSpecs.bash
 
 # grab the latest platform version from one of the new open api spec files
-newVersion=`jq -r '.info.version' ./affiliate-sdk-java/src/main/resources/openapi-spec.json`
+newVersion=`jq -r '.info.version' ./affiliate/src/main/resources/openapi-spec.json`
 
 echo "Setting the next snapshot version to $newVersion"
 mvn versions:set -DnewVersion="$newVersion-SNAPSHOT" -DgenerateBackupPoms=false
